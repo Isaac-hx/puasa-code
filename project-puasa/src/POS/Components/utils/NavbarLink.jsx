d429a456b33dba26a72db80eceb337c84ab87bb9
Isaac-hx <saydimas78@gmail.com>
2024-03-18 05:55:57 +0700

    update:color in Navbar link

--- project-puasa/src/POS/Components/utils/NavbarLink.jsx
+++ project-puasa/src/POS/Components/utils/NavbarLink.jsx
@@ -1,6 +1,6 @@
 export default function NavbarLink(props){
     return(
-        <div className='text-white hover:bg-purple-500 py-2 rounded-lg shadow'>
+        <div className='text-white hover:bg-sky-600 py-2 rounded-lg shadow'>
             <a className={`mx-auto `} href='#'>
                 {props.children}
                 <p className={`text-lg text-center  text-white`}>{props.textlink}</p>
